6b9d829f920efd903dd0cc5d980a2fb50bc3e405
vmhoang1703 <vmhoang1703@gmail.com>
2024-02-05 14:57:26 +0700

    update angular json for initializing budget

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+      apiKey: 'AIzaSyDa4fthcuCkbqzfXvFlTQbLZZdxKxxJcEg',
+      authDomain: 'phuongfashion-38f02.firebaseapp.com',
+      projectId: 'phuongfashion-38f02',
+      storageBucket: 'phuongfashion-38f02.appspot.com',
+      messagingSenderId: '73250079271',
+      appId: '1:73250079271:web:fe572f1b8fc6a89ac58402',
+    },
+    baseURL: 'https://phuong-fashion-web-app-server.onrender.com'
+  };
+  
