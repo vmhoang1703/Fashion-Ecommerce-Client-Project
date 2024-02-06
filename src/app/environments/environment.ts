8ad36a4d039dd0a5291f35128e1c5fb1b06f25b4
vmhoang1703 <vmhoang1703@gmail.com>
2024-02-06 19:33:59 +0700

    update dependancies

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -8,5 +8,6 @@ export const environment = {
     messagingSenderId: '73250079271',
     appId: '1:73250079271:web:fe572f1b8fc6a89ac58402',
   },
-  baseURL: 'https://phuong-fashion-web-app-server.onrender.com'
+  // baseURL: 'https://phuong-fashion-web-app-server.onrender.com'
+  baseURL: 'http://localhost:5000'
 };
