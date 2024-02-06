612ea854efde02eba231cf3b0debbf5d0b28f084
vmhoang1703 <vmhoang1703@gmail.com>
2024-02-06 16:05:27 +0700

    update server URL

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -8,5 +8,5 @@ export const environment = {
     messagingSenderId: '73250079271',
     appId: '1:73250079271:web:fe572f1b8fc6a89ac58402',
   },
-  baseURL: 'https://phuonngfashion-server-91f1f1ef59a4.herokuapp.com'
+  baseURL: 'https://phuong-fashion-web-app-server.onrender.com'
 };
